2eec5da506db2e438be7ee3ea7a02a602561fdcc
ClaraLasmar <114614612+ClaraLasmar@users.noreply.github.com>
2023-03-30 09:04:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-var images = [ "https://storage.googleapis.com/pod_public/1300/93631.jpg","https://media.semprefamilia.com.br/semprefamilia/2016/04/bigstock-mother-i-love-you-so-much-in-89147255-30a51300.jpg" , "https://cdn4.vectorstock.com/i/1000x1000/01/38/gamer-boy-play-game-on-computer-silhouette-vector-27880138.jpg" ,        "https://img.elo7.com.br/product/zoom/309A4A2/camiseta-adulto-significado-de-pai-dicionario-1.jpg","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.elo7.com.br%2Fcaneca-personalizada-mais-chegada-do-que-irma%2Fdp%2F12F032D&psig=AOvVaw3CO1Q8ElzMJahNGtflLm0G&ust=1677850564822000&source=images&cd=vfe&ved=0CBAQjRxqGAoTCNjNj8quvf0CFQAAAAAdAAAAABCdAQ"    ];
+var images = [ "https://storage.googleapis.com/pod_public/1300/93631.jpg",
+"https://media.semprefamilia.com.br/semprefamilia/2016/04/bigstock-mother-i-love-you-so-much-in-89147255-30a51300.jpg" ,
+ "https://media.istockphoto.com/id/601156574/vector/vector-silhouette-of-a-woman-with-a-wedding-hairstyle.jpg?s=612x612&w=0&k=20&c=kNNUm_NqbyFugjgy2gOTFpSepINg0v0GLLEvQdbMcCc=",
+        "https://img.elo7.com.br/product/zoom/309A4A2/camiseta-adulto-significado-de-pai-dicionario-1.jpg",
+    "https://media.istockphoto.com/id/601156574/vector/vector-silhouette-of-a-woman-with-a-wedding-hairstyle.jpg?s=612x612&w=0&k=20&c=kNNUm_NqbyFugjgy2gOTFpSepINg0v0GLLEvQdbMcCc="
+    ];
            
 var names = ["","Mãe ", "Eu", "Pai","Irmã"];
           
 var i = 0;
 function update()
-
 {
      i++;
      
